97ebcdeab7a4aaa4e9e26f3574b344592d2c3636
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-01 19:15:29 +0300

    linked list

--- linkedlist/linked.cpp
+++ linkedlist/linked.cpp
@@ -13,7 +13,6 @@ struct ListNode* head = NULL;
 //function to insert a node to head of a linked list
 void insert(int new_data)
 {
-    cout << "inserting new data " << endl;
     struct ListNode* new_node = (struct ListNode*) malloc(sizeof(struct ListNode*));
     //insert the new_data to the new_node
     new_node->val = new_data;
@@ -21,18 +20,31 @@ void insert(int new_data)
     head = new_node;
 }
 
+void traverse(ListNode* head)
+{
+    while(head != NULL)
+    {
+        cout << head->val << endl;
+        head = head->next;
+    }
+}
+
 int main()
 {
+    cout << "Linked list in c++"<< endl;
     //lets read in input to add to the node
     //int n 
     //the input will be in the format 1 2 3 4 5
+    cout << "Enter the data to insert " << endl;
     int n;
+    cin>>n;
     for(int i=0; i<n; i++)
     {
         int v;
         cin>>v;
         insert(v);
     }
-    cout << "Linked list in c++"<< endl;
+    cout << "printing the values now " << endl;
+    traverse(head);
 }
 
