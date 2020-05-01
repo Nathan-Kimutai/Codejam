7e5499aac81b7287265555db2657d3714b595983
Nathan Kimutai <nathanpykimutai@gmail.com>
2020-05-01 18:48:13 +0300

    linked list

---
+++ linkedlist/linked.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//use structs to represent ListNodes
+struct ListNode {
+				int val;
+				sturct ListNode *next;
+};
+
+//function to insert a node to head of a linked list
+
+int main()
+{
+				cout << "Linked list in c++"<< endl;
+}
